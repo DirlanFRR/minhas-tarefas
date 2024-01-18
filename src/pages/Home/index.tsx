df21c72463a28abb0276d0ef6fc02f97afa29e05
Dirlan Fernando <dirlanfr@gmail.com>
2024-01-18 18:50:35 -0300

    instalar a biblioteca React Router compreender o conceito de Single Page Application. conhecer funções, constantes, componentes, elementos, propriedades usadas no React Router.

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import BarraLateral from '../../containers/BarraLateral'
+import ListadeTarefas from '../../containers/ListadeTarefas'
+
+const Home = () => (
+  <>
+    <BarraLateral />
+    <ListadeTarefas />
+  </>
+)
+
+export default Home
